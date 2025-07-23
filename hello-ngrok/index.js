64c4bad831de9d5b6f3d2840b844ed81c26c4e20
Thiago toffanetto <app.zapconversa@gmail.com>
2025-07-23 13:42:10 +0000

    neste caso, nao preciso é ajustar o index?
    const http = require('http')

--- hello-ngrok/index.js
+++ hello-ngrok/index.js
@@ -1,12 +1,15 @@
-const http = require('http');
 const ngrok = require('@ngrok/ngrok');
 
-// Create webserver
-http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end('Congrats you have created an ngrok web server');
-}).listen(8080, () => console.log('Node.js web server at 8080 is running...'));
+// Este script agora apenas inicia o túnel do ngrok e o aponta
+// para a porta 9002, onde a aplicação principal do Next.js está rodando.
+// O servidor de teste antigo na porta 8080 foi removido.
+
+console.log('Iniciando túnel do ngrok para a porta 9002...');
 
 // Get your endpoint online
-ngrok.connect({ addr: 8080, authtoken_from_env: true })
-	.then(listener => console.log(`Ingress established at: ${listener.url()}`));
+ngrok.connect({ addr: 9002, authtoken_from_env: true })
+	.then(listener => console.log(`Túnel estabelecido em: ${listener.url()}`))
+    .catch(err => {
+        console.error('Erro ao iniciar o ngrok:', err);
+        process.exit(1);
+    });
